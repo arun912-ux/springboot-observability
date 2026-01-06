7be9c082cb3962b57215dde11255cfd71c732f61
arun912-ux <arunshanagonda777@gmail.com>
2026-01-06 17:56:21 +0530

    observability - logs working.

--- src/main/java/org/example/observability/config/InstallOpenTelemetryAppender.java
+++ src/main/java/org/example/observability/config/InstallOpenTelemetryAppender.java
@@ -2,9 +2,11 @@ package org.example.observability.config;
 
 import io.opentelemetry.api.OpenTelemetry;
 import io.opentelemetry.instrumentation.logback.appender.v1_0.OpenTelemetryAppender;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 class InstallOpenTelemetryAppender implements InitializingBean {
 
@@ -14,9 +16,9 @@ class InstallOpenTelemetryAppender implements InitializingBean {
         this.openTelemetry = openTelemetry;
     }
 
-
     @Override
     public void afterPropertiesSet() {
+        log.info("OpenTelemetryAppender initialized : {}", openTelemetry);
         OpenTelemetryAppender.install(this.openTelemetry);
     }
 
